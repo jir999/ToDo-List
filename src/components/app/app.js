54f1a9ee882b69d6433f03647bbb6c08d88a40ad
jir999 <jir.avetisyan@gmail.com>
2020-06-03 21:03:52 +0400

    ItemStatusFilter // All, Active, Done

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ class App extends Component {
         this.createToDoItem("Make Awesome App"),
         this.createToDoItem("Have a lunch")
       ],
-      term: ''
+      term: '',
+      filter: 'all'      // active, all, done 
     }
 
     createToDoItem(label){
@@ -108,6 +109,10 @@ class App extends Component {
         this.setState({term});
     }
 
+    onFilterChange = (filter) => {
+        this.setState({filter});
+    }
+
     search(items, term){
         if(term.length === 0){
             return items;
@@ -117,10 +122,23 @@ class App extends Component {
         });
     }
 
-    render(){
-        const { todoData, term } = this.state;
+    filter(items, filter){
+        switch(filter){
+            case 'all' :
+                return items;
+            case 'active' :
+                return items.filter((item) => !item.done);
+            case 'done' :
+                return items.filter((item) => item.done);
+            default:
+                return items;
+        }
+    }
 
-        const visibleItems = this.search(todoData, term);
+    render(){
+        const { todoData, term, filter } = this.state;
+
+        const visibleItems = this.filter(this.search(todoData, term) , filter);
 
         const doneCount = todoData.filter((el) => el.done).length;
         const todoCount = todoData.length - doneCount;
@@ -130,7 +148,8 @@ class App extends Component {
               <AppHeader toDo={todoCount} done={doneCount} />
               <div className="top-panel d-flex">
                   <SearchPanel onSearchChange={this.onSearchChange} />
-                  <ItemStatusFilter />
+                  <ItemStatusFilter filter={filter} 
+                                    onFilterChange={this.onFilterChange} />
               </div>
               <TodoList todos={visibleItems} 
                         onDeleted={ this.deleteItem } 
